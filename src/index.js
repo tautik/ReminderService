0f6355b2967a40213e581825d635ae6922abacef
Tautik Agrahari <tautikagrahari@gmail.com>
2023-03-13 12:49:13 +0530

    Test purpose: to send mail every 2min used cron

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const { PORT } = require("./config/serverConfig");
 
 const { sendBasicEmail } = require("./services/email-service");
 
+//to send mail in interval of time
+var cron = require("node-cron");
+
 const setupAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
@@ -14,12 +17,17 @@ const setupAndStartServer = () => {
     console.log(`Server started at port ${PORT}`);
 
     //calling basic email-service and passing this message to send mail
-    sendBasicEmail(
-      "support@admin.com",
-      "moviebookingappservice@gmail.com",
-      "This is a testing email",
-      "Hey, how are you, I hope you like the support"
-    );
+
+    cron.schedule("*/2 * * * *", () => {
+      console.log("running a task every two minutes");
+
+      sendBasicEmail(
+        "support@admin.com",
+        "moviebookingappservice@gmail.com",
+        "This is a testing email",
+        "Hey, how are you, I hope you like the support"
+      );
+    });
   });
 };
 
