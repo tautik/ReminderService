8995d9d76ec3b5f0bb86e776b9665047036640ec
Tautik Agrahari <tautikagrahari@gmail.com>
2023-03-17 21:05:31 +0530

    temp commit: ISSUE rabbitMQ not connecting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const { PORT } = require("./config/serverConfig");
+const { PORT, REMINDER_BINDING_KEY } = require("./config/serverConfig");
 
 // const { sendBasicEmail } = require('./services/email-service');
 const TicketController = require("./controllers/ticket-controller");
 
 const jobs = require("./utils/job");
+const { createChannel, subscribeMessage } = require("./utils/messageQueue");
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.post("/api/v1/tickets", TicketController.create);
+
+  //suscribing message
+  const channel = await createChannel();
+  subscribeMessage(channel, undefined, REMINDER_BINDING_KEY);
 
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
